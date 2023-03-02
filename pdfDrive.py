edb88771883852f105f89bb2e64deb618980cdba
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-03-02 14:21:04 +0000

    Add files via upload

--- pdfDrive.py
+++ pdfDrive.py
@@ -1,9 +1,10 @@
-import datetime
+"""
+Written by Benjamin Jack Cullen aka Holographic_Sol
+"""
+
 import os
 import sys
 import time
-
-import handler_chunk
 import pdfDriveTool
 import socket
 import urllib3
@@ -11,40 +12,29 @@ import datetime
 
 
 socket.setdefaulttimeout(10)
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-                  '73.0.3683.103 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/'
-              'signed-exchange;v=b3',
-    'Accept-Encoding': 'text/plain',
-    'Accept-Language': 'en-US,en;q=0.9'
-    }
 
 
 def get_dt():
     return str(f'[{str(datetime.datetime.now())}]')
 
 
-def download(url, fname):
+def download(url: str, fname: str):
     _download_finished = False
     _data = bytes()
-
     try:
         http = urllib3.PoolManager(retries=5)
-        r = http.request('GET', url, preload_content=False, headers=headers)
+        r = http.request('GET', url, preload_content=False)
         while True:
             data = r.read(1024)
             if data:
                 _data += data
             else:
                 _download_finished = True
-
                 open(fname, 'w').close()
                 with open(fname, 'wb') as out:
                     out.write(_data)
                 out.close()
                 break
-
     except Exception as e:
         print(f'{get_dt()} {e}')
     try:
@@ -55,27 +45,27 @@ def download(url, fname):
     if _download_finished is False:
         print(f'{get_dt()} Retrying:', url)
         time.sleep(5)
-        download(url, fname)
+        download(url=url, fname=fname)
 
 
-def downloader(book_urls, _search_q, _i_page, _max_page):
+def downloader(_book_urls: list, _search_q: str, _i_page: str, _max_page: str):
     i_progress = 1
-    for book_url in book_urls:
+    for book_url in _book_urls:
         print('_'*50)
-        print(f'{get_dt()} Progress: {i_progress}/{len(book_urls)} ({_i_page}/{_max_page})')
+        print(f'{get_dt()} Progress: {i_progress}/{len(_book_urls)} ({_i_page}/{_max_page})')
 
         if not os.path.exists('./library/'):
             os.mkdir('./library/')
         if not os.path.exists('./library/' + _search_q):
             os.mkdir('./library/' + _search_q)
 
-        fname = './library/' + _search_q + '/' + pdfDriveTool.make_file_name(book_url)
+        fname = './library/' + _search_q + '/' + pdfDriveTool.make_file_name(book_url=book_url)
         if not os.path.exists(fname):
             print(f'{get_dt()} Enumerating: {book_url}')
             url = pdfDriveTool.enumerate_download_link(url=book_url)
             if url:
                 print(f'{get_dt()} Enumeration result: {url}')
-                download(url, fname)
+                download(url=url, fname=fname)
             else:
                 print(f'{get_dt()} URL: Unpopulated')
 
@@ -113,7 +103,7 @@ for i in range(1, int(_max_page)):
 
     """ Download """
     print(f'{get_dt()} Starting downloads..')
-    downloader(book_urls, _search_q=_search_q, _i_page=i_page, _max_page=_max_page)
+    downloader(_book_urls=book_urls, _search_q=_search_q, _i_page=str(i_page), _max_page=str(_max_page))
     print('')
 
     i_page += 1
--- pdfDriveTool.py
+++ pdfDriveTool.py
@@ -1,22 +1,20 @@
 """
 Written by Benjamin Jack Cullen aka Holographic_Sol
 """
-import time
+
 import socket
 import requests
 from bs4 import BeautifulSoup
 import datetime
-from fake_useragent import UserAgent
 
 socket.setdefaulttimeout(10)
-ua = UserAgent()
 
 
-def get_dt():
+def get_dt() -> str:
     return str(f'[{str(datetime.datetime.now())}]')
 
 
-def get_pages(search_q: str):
+def get_pages(search_q: str) -> str:
     max_page = 0
     rHead = requests.get('https://www.pdfdrive.com/search?q=' + search_q)
     data = rHead.text
@@ -33,17 +31,16 @@ def get_pages(search_q: str):
                 page = int(str(href).replace(f'/search?q={search_q}&page=', ''))
                 if page > max_page:
                     max_page = page
-        except:
+        except Exception as e:
+            print(f'{get_dt()} {e}')
             max_page = 1
     return str(max_page)
 
 
-def get_link(url):
+def get_link(url: str) -> list:
     book_urls = []
     try:
-        headers = {'User-Agent': str(ua.random)}
-
-        rHead = requests.get(url, headers=headers, timeout=5)
+        rHead = requests.get(url, timeout=5)
         data = rHead.text
         soup = BeautifulSoup(data, "html.parser")
         for link in soup.find_all('a'):
@@ -60,39 +57,23 @@ def get_link(url):
     return book_urls
 
 
-def get_all_page_links(search_q: str, max_page: str):
-    book_urls = []
-    i_page = 1
-    for i in range(i_page, int(max_page)):
-        url = 'https://www.pdfdrive.com/search?q=' + str(search_q).replace(' ', '+') + '&pagecount=&pubyear=&searchin=&page='+str(i_page)
-        print(f'{get_dt()} Scanning page: {url}')
-        book_urls.append(get_link(url))
-        time.sleep(3)
-        i_page += 1
-    return book_urls
-
-
-def get_page_links(search_q: str, page: str):
-
-    url = 'https://www.pdfdrive.com/search?q=' + str(search_q).replace(' ', '+') + '&pagecount=&pubyear=&searchin=&page='+str(page)
+def get_page_links(search_q: str, page: str) -> list:
+    url = str('https://www.pdfdrive.com/search?q=' + str(search_q).replace(' ', '+') + '&pagecount=&pubyear=&searchin=&page='+str(page))
     print(f'{get_dt()} Scanning page: {url}')
+    return get_link(url=url)
 
-    return get_link(url)
 
-
-def enumerate_download_link(url):
+def enumerate_download_link(url: str) -> str:
     try:
         rHead = requests.get(url)
         data = rHead.text
         soup = BeautifulSoup(data, "html.parser")
-
         data_preview = ''
         for link in soup.find_all('button'):
             data_preview = link.get('data-preview')
             if data_preview is not None:
                 data_preview = data_preview
                 break
-
         url = ''
         if data_preview:
             data_preview = data_preview.replace('/ebook/preview?id=', '').replace('&session=', ' ')
@@ -102,10 +83,10 @@ def enumerate_download_link(url):
             url = f'https://www.pdfdrive.com//download.pdf?id={data_id}&h={h_id}&u=cache&ext=pdf'
     except Exception as e:
         print(f'{get_dt()} {e}')
-        enumerate_download_link(url)
+        enumerate_download_link(url=url)
 
     return url
 
 
-def make_file_name(book_url):
+def make_file_name(book_url: str) -> str:
     return book_url.replace('https://www.pdfdrive.com//', '').replace('.html', '.pdf')
