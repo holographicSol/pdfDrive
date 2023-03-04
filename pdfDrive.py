7214533caefcc2f12087ac687c395db1d8abac60
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-03-04 17:05:39 +0000

    Add files via upload

--- pdfDrive.py
+++ pdfDrive.py
@@ -139,7 +139,7 @@ def downloader(_book_urls: list, _search_q: str, _i_page: str, _max_page: str):
                     download(url=url, fname=fname)
                 else:
                     print(f'{get_dt()} ' + color('[URL] Unpopulated.', c='Y'))
-                print(f'{get_dt()} ' + color('[Skipping] ', c='LC') + color('File exists in records.', c='W'))
+                    print(f'{get_dt()} ' + color('[Skipping] ', c='LC') + color('File exists in records.', c='W'))
         else:
             print(f'{get_dt()} ' + color('[Skipping] ', c='LC') + color('File already exists in filesystem.', c='W'))
 
