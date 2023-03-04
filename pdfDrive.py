ee97c308f1ad71bef16d25411f907833de9846c0
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-03-04 14:27:05 +0000

    Add files via upload

--- pdfDrive.py
+++ pdfDrive.py
@@ -76,13 +76,12 @@ def download(url: str, fname: str):
 
     if os.path.exists(fname):
         if os.path.getsize(fname) > 100:
-            print(f'{get_dt()} ' + color('Downloaded Successfully.', c='G'))
+            print(f'{get_dt()} ' + color('[Downloaded Successfully]', c='G'))
             with codecs.open('./books_saved.txt', 'a+', encoding='utf8') as fo:
                 fo.write(fname+'\n')
             fo.close()
         else:
-            print(f'{get_dt()} ' + color('File less than 100 bytes.', c='Y'))
-            print(f'{get_dt()} ' + color('Download Failed.', c='R'))
+            print(f'{get_dt()} ' + color('[Download Failed] File less than 100 bytes.', c='R'))
             if fname not in failed_downloads:
                 with codecs.open('./books_failed.txt', 'a+', encoding='utf8') as fo:
                     fo.write(fname+'\n')
@@ -90,8 +89,7 @@ def download(url: str, fname: str):
                 failed_downloads.append(fname)
             os.remove(fname)
     else:
-        print(f'{get_dt()} ' + color('File did not save.', c='Y'))
-        print(f'{get_dt()} ' + color('Download Failed.', c='R'))
+        print(f'{get_dt()} ' + color('[Download Failed] File did not save.', c='R'))
         if fname not in failed_downloads:
             with codecs.open('./books_failed.txt', 'a+', encoding='utf8') as fo:
                 fo.write(fname + '\n')
@@ -107,29 +105,29 @@ def downloader(_book_urls: list, _search_q: str, _i_page: str, _max_page: str):
         retry_max = 3
         print('_'*50)
         print('')
-        print(f'{get_dt()} Progress: {i_progress}/{len(_book_urls)} ({_i_page}/{_max_page})')
-        print(f'{get_dt()} Category: {_search_q}')
+        print(f'{get_dt()} [Progress] {i_progress}/{len(_book_urls)} ({_i_page}/{_max_page})')
+        print(f'{get_dt()} [Category] {_search_q}')
         if not os.path.exists('./library/'):
             os.mkdir('./library/')
         if not os.path.exists('./library/' + _search_q):
             os.mkdir('./library/' + _search_q)
 
         fname = './library/' + _search_q + '/' + pdfDriveTool.make_file_name(book_url=book_url)
-        print(f'{get_dt()} Book: {fname}')
+        print(f'{get_dt()} [Book] {fname}')
         if not os.path.exists(fname):
             if fname not in success_downloads:
-                print(f'{get_dt()} Enumerating: {book_url}')
+                print(f'{get_dt()} [Enumerating] {book_url}')
                 url = pdfDriveTool.enumerate_download_link(url=book_url)
                 if url:
-                    print(f'{get_dt()} Enumeration result: {url}')
+                    print(f'{get_dt()} [Enumeration result] {url}')
+                    print(f'{get_dt()} [Downloading]')
                     download(url=url, fname=fname)
                 else:
-                    print(f'{get_dt()} URL: Unpopulated')
-                    print(f'{get_dt()} ' + color('URL Unpopulated.', c='Y'))
+                    print(f'{get_dt()} ' + color('[URL] Unpopulated.', c='Y'))
             else:
-                print(f'{get_dt()} ' + color('Skipping (File exists in records):', c='Y') + str(book_url))
+                print(f'{get_dt()} ' + color('[Skipping] (File exists in records): ', c='Y') + str(book_url))
         else:
-            print(f'{get_dt()} ' + color('Skipping (File already exists):', c='Y') + str(book_url))
+            print(f'{get_dt()} ' + color('[Skipping] (File already exists): ', c='Y') + str(book_url))
 
         i_progress += 1
 
@@ -156,7 +154,7 @@ else:
             _search_q = _search_q + ' ' + x
         i += 1
     _search_q = _search_q[1:]
-    print(f'{get_dt()} Search:', _search_q)
+    print(f'{get_dt()} [Search]', _search_q)
 
     """ Max Pages """
     _max_page = 1
@@ -165,10 +163,10 @@ else:
         _max_page = int(stdin[idx])
     else:
         _max_page = pdfDriveTool.get_pages(search_q=_search_q)
-    print(f'{get_dt()} Pages: {_max_page}')
+    print(f'{get_dt()} [Pages] {_max_page}')
 
     """ Scan Pages for book URLSs """
-    print(f'{get_dt()} Getting book links: (this may take a moment)')
+    print(f'{get_dt()} [Getting book links] This may take a moment..')
 
     with codecs.open('./books_failed.txt', 'r+', encoding='utf8') as fo:
         for line in fo:
@@ -187,14 +185,14 @@ else:
     for i in range(1, int(_max_page)):
         if i_page >= i:
             book_urls = pdfDriveTool.get_page_links(search_q=_search_q, page=str(i_page))
-            print(f'{get_dt()} Book URLs: {book_urls}')
-            print(f'{get_dt()} Books: {len(book_urls)}')
+            print(f'{get_dt()} [Book URLs] {book_urls}')
+            print(f'{get_dt()} [Books] {len(book_urls)}')
 
             """ Download """
-            print(f'{get_dt()} Starting downloads..')
+            print(f'{get_dt()} [Starting Download]')
             downloader(_book_urls=book_urls, _search_q=_search_q, _i_page=str(i_page), _max_page=str(_max_page))
             print('')
         else:
-            print(f'{get_dt()} Skipping page: {i_page}')
+            print(f'{get_dt()} [Skipping Page] {i_page}')
 
         i_page += 1
