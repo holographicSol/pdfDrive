913ed8183d9a61a9fe217940a16b04a15eb87ed7
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-26 13:42:39 +0100

    Add files via upload

--- pdfDrive.py
+++ pdfDrive.py
@@ -119,7 +119,8 @@ def download_file(url: str, fname: str):
 
         # check: temporary file worth keeping? (<1024 bytes would be less than 1024 characters, reduce this if needed)
         # - sometimes file exists on a different server, this software does not intentionally follow any external links,
-        # - if the file is in another place then a very small file may be downloaded and will then be detected/deleted.
+        # - if the file is in another place then a very small file may be downloaded because ultimately the file we
+        #   wanted was not present and will then be detected and deleted.
         if os.path.getsize(fname+'.tmp') >= 1024:
 
             # create final download file from temporary file
