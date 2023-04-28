2fe1460980890cab8eb9d2680bf4993254794591
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-28 09:52:04 +0100

    Add files via upload

--- requirements.bat
+++ requirements.bat
@@ -1,4 +1,7 @@
-pip install fake_useragent
-pip install requests
-pip install colorama
 pip install bs4
+pip install pyqt5
+pip install asyncio
+pip install aiohttp
+pip install aiofiles
+pip install colorama
+pip install fake_useragent
